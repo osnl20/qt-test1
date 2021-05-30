8d8ca24b87c462d0ab6acf6f85d5681321bbc541
Oleg Shchukin <oleg.shchukin@natlex.ru>
2021-05-30 19:49:28 +0300

    fix function

--- app/quadratic.c
+++ app/quadratic.c
@@ -2,9 +2,16 @@
 
 Roots quadratic(double a, double b, double c)
 {
-    Roots result;
-    result.rootsCount = 2;
-    result.x1 = a+b+c;
-    result.x2 = 0.0;
+    Roots result = {.rootsCount = 0, .x1 = 0.0, .x2 = 0.0};
+    double d = b*b - 4*a*c;
+    if( d > 0.0 ) {
+        result.rootsCount = 2;
+        result.x1 = (-b-d)/a/2.0;
+        result.x2 = (-b+d)/a/2.0;
+    } if ( d == 0.0 ) {
+        result.rootsCount = 1;
+        result.x1 = result.x2 = -b/a/2.0;
+    }
+
     return result;
 }
