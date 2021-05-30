399545272b256edad8d6fb623909cee4d612c67a
Oleg Shchukin <oleg.shchukin@natlex.ru>
2021-05-30 19:58:46 +0300

    add more tests

--- tests/quadratic_test.h
+++ tests/quadratic_test.h
@@ -6,7 +6,7 @@ extern "C" {
 #include "quadratic.h"
 }
 
-TEST(test1, quadratic)
+TEST(quadraticTest, twoRoots)
 {
     // Given
     double a = 1.0, b = -3.0, c = 2.0;
@@ -18,4 +18,30 @@ TEST(test1, quadratic)
     EXPECT_EQ(2, res.rootsCount);
     EXPECT_EQ(1.0, res.x1);
     EXPECT_EQ(2.0, res.x2);
+}
+
+TEST(quadraticTest, noRoots)
+{
+    // Given
+    double a = 3.0, b = 11.0, c = 19.0;
+
+    // When
+    Roots res = quadratic(a, b, c);
+
+    // Then
+    EXPECT_EQ(0, res.rootsCount);
+}
+
+TEST(quadraticTest, oneRoot)
+{
+    // Given
+    double a = 9.0, b = 54.0, c = 81.0;
+
+    // When
+    Roots res = quadratic(a, b, c);
+
+    // Then
+    EXPECT_EQ(1, res.rootsCount);
+    EXPECT_EQ(-3.0, res.x1);
+    EXPECT_EQ(-3.0, res.x2);
 }
