8b912483e1eb33418605c8e8c8e1a3c33fd73058
Oleg Shchukin <oleg.shchukin@natlex.ru>
2021-05-30 20:52:56 +0300

    add more tests

--- app/quadratic.c
+++ app/quadratic.c
@@ -1,4 +1,12 @@
+#include <math.h>
+
 #include "quadratic.h"
+
+void swap(double *a, double *b) {
+    double c = *a;
+    *a = *b;
+    *b = c;
+}
 
 Roots quadratic(double a, double b, double c)
 {
@@ -6,8 +14,11 @@ Roots quadratic(double a, double b, double c)
     double d = b*b - 4*a*c;
     if( d > 0.0 ) {
         result.rootsCount = 2;
-        result.x1 = (-b-d)/a/2.0;
-        result.x2 = (-b+d)/a/2.0;
+        result.x1 = (-b-sqrt(d))/a/2.0;
+        result.x2 = (-b+sqrt(d))/a/2.0;
+        if(result.x1 > result.x2) {
+            swap(&result.x1, &result.x2);
+        }
     } else if ( d == 0.0 ) {
         result.rootsCount = 1;
         result.x1 = result.x2 = -b/a/2.0;
